972c0f88d671bb93ab97d54e967b85542353de95
intanfazirah <intanfazirah.ramli@yahoo.com>
2025-05-26 18:07:48 +0800

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:t_store/app.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+/// Entry point of Flutter App
+Future<void> main() async {
+
+  //Todo: Add Widgets Binding
+  //Todo: Init Local Storage
+  //Todo: Init Payment Methods
+  //Todo: Await Native Splash
+
+  //Todo: Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
+  //Todo: Initialize Authentication
+
+
   runApp(const App());
 }
 
